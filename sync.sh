bdfb5a4f50feda9c6cc5a90ea0e8c7dfb0273d89
gffuma <fumagalli.gf@gmail.com>
2015-07-18 14:30:25 +0200

    Adding sync file

---
+++ sync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dir=~/dotfiles                    # dotfiles directory
+files="vimrc zshrc tmux tmux.conf"    # list of files/folders to symlink in homedir
+
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+for file in $files; do
+	echo "Killing $file"
+	rm -rf ~/.$file
+	echo "Creating symlink to $file in home directory."
+	ln -s $dir/.$file ~/.$file
+done
